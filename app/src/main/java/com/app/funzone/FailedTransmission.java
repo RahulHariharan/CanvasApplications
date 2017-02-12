bbbccbed4ff2be6d6d479acf3b1bb40f2ffd8cf3
Rahul-Hariharan <fullyautomatix.inc@gmail.com>
2017-02-12 21:48:58 +0530

    Sprinkles
    Draws random dots on the surface view.

---
+++ app/src/main/java/com/app/funzone/FailedTransmission.java
@@ -0,0 +1,66 @@
+package com.app.funzone;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.Random;
+
+/**
+ * Created by rahulhariharan on 12/02/17.
+ */
+
+public class FailedTransmission extends SurfaceView {
+
+    public FailedTransmission(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+
+    private void init() {
+        SurfaceHolder holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                Canvas canvas = surfaceHolder.lockCanvas();
+                drawSomething(canvas);
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
+    }
+
+    private void drawSomething(Canvas canvas) {
+        int strokeWidth = 200;
+        Paint paint = new Paint();
+        paint.setStrokeWidth(strokeWidth);
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
+        Random random = new Random();
+        int numberOfLines = width / strokeWidth;
+        int[] colors = {Color.BLUE, Color.MAGENTA, Color.YELLOW, Color.RED, Color.CYAN, Color.DKGRAY, Color.GREEN, Color.LTGRAY};
+        int x_start = 0;
+        int y_start = 0;
+        for(int i=0; i <= numberOfLines; i++) {
+            int index = random.nextInt(colors.length);
+            paint.setColor(colors[index]);
+            canvas.drawRect(x_start, y_start, x_start + strokeWidth, height, paint);
+            x_start += strokeWidth;
+        }
+    }
+
+}
--- app/src/main/java/com/app/funzone/Sprinkles.java
+++ app/src/main/java/com/app/funzone/Sprinkles.java
@@ -14,13 +14,11 @@ import java.util.Random;
  * Created by rahulhariharan on 12/02/17.
  */
 
-public class MySurfaceView extends SurfaceView {
-
-    public MySurfaceView(Context context, AttributeSet attrs) {
+public class Sprinkles extends SurfaceView {
+    public Sprinkles(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
     }
-
 
     private void init() {
         SurfaceHolder holder = getHolder();
@@ -45,22 +43,24 @@ public class MySurfaceView extends SurfaceView {
     }
 
     private void drawSomething(Canvas canvas) {
-        int strokeWidth = 200;
         Paint paint = new Paint();
-        paint.setStrokeWidth(strokeWidth);
+
         int width = canvas.getWidth();
         int height = canvas.getHeight();
-        Random random = new Random();
-        int numberOfLines = width / strokeWidth;
+
+        int numberOfPoints = 100000;
+
         int[] colors = {Color.BLUE, Color.MAGENTA, Color.YELLOW, Color.RED, Color.CYAN, Color.DKGRAY, Color.GREEN, Color.LTGRAY};
-        int x_start = 0;
-        int y_start = 0;
-        for(int i=0; i <= numberOfLines; i++) {
+        Random random = new Random();
+        paint.setStrokeWidth(5);
+        canvas.drawLine(100, 100, 0, 0, paint);
+
+        for(int i=0; i<numberOfPoints; i++) {
             int index = random.nextInt(colors.length);
             paint.setColor(colors[index]);
-            canvas.drawRect(x_start, y_start, x_start + strokeWidth, height, paint);
-            x_start += strokeWidth;
+            int x = random.nextInt(width);
+            int y = random.nextInt(height);
+            canvas.drawPoint(x, y, paint);
         }
     }
-
 }
